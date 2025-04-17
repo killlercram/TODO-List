3dad18bc9e1ad1cd8bb6278226eeaa8462f6d078
killlercam <herosviral@gmail.com>
2025-04-17 12:56:10 +0530

    Connected with database and fixed some issues

--- server.js
+++ server.js
@@ -2,8 +2,10 @@
 require("dotenv").config();
 
 const app = require("./app");
+const dbConfig = require("./src/config/dbConfig");
+
 const port = process.env.PORT_NUMBER || 3000;
 
 app.listen(port, () => {
-  console.log(`Server started at port ${port}`);
+  console.log(`Server started at port: ${port}`);
 });
---
+++ src/config/dbConfig.js
@@ -0,0 +1,24 @@
+//Connecting it with the mongodb database
+require("dotenv").config();
+const mongoose = require("mongoose");
+
+const MONGODB__CONNECTION_URL = process.env.MONGO_URL;
+
+//Establishing Connection
+ mongoose.connect(MONGODB__CONNECTION_URL);
+
+//getting connection state
+const db = mongoose.connection;
+
+//Checking the response based on the state
+db.on("connected", () =>{
+  console.log("Db Connection Successfull!!");
+});
+db.on("error", () =>{
+  console.log("Db Connection Failed!!");
+});
+db.on("disconnected", () => {
+  console.log("MongoDB disconnected.");
+});
+
+module.exports = db;
